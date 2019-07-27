c16c91d5357fdf3905a58b5b098d781e21443ec2
1270355646@qq.com <1270355646@qq.com>
2019-07-27 11:15:19 +0800

    bodychecktable

--- src/components/bodycheck/BodyCheck.vue
+++ src/components/bodycheck/BodyCheck.vue
@@ -1,15 +1,15 @@
 <template>   
 
 <table>
-    <br>
-    <h1 class="titleName">{{ name }}的体检报告</h1>
-    <tr>
-        <td>年龄</td>
-        <td>18</td>
+    
+    <tr  class="titleName">
+        
+        <td colspan="2"><img src="../../images/picture1.png" alt="">{{ name }}的体检报告</td>
     </tr>
-     <tr>
-        <td>性别</td>
-        <td>女</td>
+   
+    <tr>
+        <td>年龄性别</td>
+        <td>18（女）</td>
     </tr>
      <tr>
         <td>体重</td>
@@ -18,6 +18,18 @@
      <tr>
         <td>身高</td>
         <td>160</td>
+    </tr>
+    <tr>
+        <td>体温</td>
+        <td>36</td>
+    </tr>
+    <tr>
+        <td>口部图像</td>
+        <td><img src="../../images/mouth.jpg" alt=""></td>
+    </tr>
+    <tr>
+        <td>手部图像</td>
+        <td><img src="../../images/hand.png" alt=""></td>
     </tr>
 </table>
  
@@ -40,28 +52,44 @@ export default {
 
 <style lang="scss" scoped>
 table{
+   
+  margin-top: 10px;
+    width: 100%;
+    border:1px solid;
     
-    width:100%;
+  
 
     .titleName{
          width: 100%;
-         font-size: 20px;
-         height: 30px;
-         text-align: center;
+         font-size: 25px;
+         height: 70px;
+         line-height:70px;
+        //  vertical-align: middle
          
+         img{
+             width: 40px;
+             height: 60px;
+             margin-right: 20px;
+             margin-bottom:-20px;
+         }
     }
    
     tr{
-      border: 3 black;
+      border: 3px black;
       height: 50px;
       width: 100%;
-      background-color: rgb(255,192,0);
+      background-color: rgb(255, 206, 58);
       margin: 5px;
       padding: 5px;  
 
      td{
+        border:3px solid white;
         text-align:center;
         width:50%;
+        img{
+            width: 100px;
+            height: 100px;
+        }
        
      }
 
