1f8dd5fdfce77d6cc4e894e25bf2781888525799
1270355646@qq.com <1270355646@qq.com>
2020-01-20 19:44:50 +0800

    change

--- src/components/weather/Weather.vue
+++ src/components/weather/Weather.vue
@@ -1,14 +1,20 @@
 <template>
 <div>
-    <iframe width="100%" height="600px" src="https://widget-page.heweather.net/h5/index.html?bg=1&md=0123456&lc=auto&key=f6a379fdc520484cab0e0f7726d27959" frameborder="0">weather</iframe>
+    <iframe width="100%" height="100%"
+    src="https://widget-page.heweather.net/h5/index.html?bg=1&md=0123456&lc=auto&key=f6a379fdc520484cab0e0f7726d27959"  
+    frameborder="0" scrolling="auto" name="_blank" id="_blank" >weather</iframe>
 
     </div>
 </template>
 
 <script>
-export default {
-    
-}
+
+//     (function adjustIframe(){
+//         var ifm= document.getElementById("bi_iframe");
+//         ifm.height=document.documentElement.clientHeight;
+//         ifm.width=document.documentElement.clientWidth;
+// })()
+
 </script>
 
 <style lang="scss" scoped>
